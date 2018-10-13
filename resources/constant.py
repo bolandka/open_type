133b857238fd6eb43f26403ef2bee1ebce1e10b9
bolandka <katarina.boland@gesis.org>
2018-10-13 11:22:53 +0200

    generalize paths

--- resources/constant.py
+++ resources/constant.py
@@ -1,4 +1,5 @@
 from collections import namedtuple, defaultdict
+import os
 
 def load_vocab_dict(vocab_file_name, vocab_max_size=None, start_vocab_count=None):
   with open(vocab_file_name) as f:
@@ -11,16 +12,17 @@ def load_vocab_dict(vocab_file_name, vocab_max_size=None, start_vocab_count=None
       file_content = dict(zip(text, range(0, len(text))))
   return file_content
 
-FILE_ROOT = '/home/eunsol/Data/entity_type_data/release/'
-GLOVE_VEC = '/home/eunsol/Data/pretrainedVector/glove.840B.300d.txt'
-EXP_ROOT = '/home/eunsol/Experiment/pytorch/'
+BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+FILE_ROOT = os.path.join(BASE_PATH, 'release/')
+GLOVE_VEC = os.path.join(BASE_PATH, 'vectors/glove.840B.300d.txt')
+EXP_ROOT = os.path.join('models/')
 
 ANSWER_NUM_DICT = {"open": 10331, "onto":89, "wiki": 4600, "kb":130, "gen":9}
 
-KB_VOCAB = load_vocab_dict(FILE_ROOT + "/ontology/types.txt", 130)
-WIKI_VOCAB = load_vocab_dict(FILE_ROOT + "/ontology/types.txt", 4600)
-ANSWER_VOCAB = load_vocab_dict(FILE_ROOT + "/ontology/types.txt")
-ONTO_ANS_VOCAB = load_vocab_dict(FILE_ROOT + '/ontology/onto_ontology.txt')
+KB_VOCAB = load_vocab_dict(os.path.join(FILE_ROOT, "ontology/types.txt"), 130)
+WIKI_VOCAB = load_vocab_dict(os.path.join(FILE_ROOT, "ontology/types.txt"), 4600)
+ANSWER_VOCAB = load_vocab_dict(os.path.join(FILE_ROOT, "ontology/types.txt"))
+ONTO_ANS_VOCAB = load_vocab_dict(os.path.join(FILE_ROOT, 'ontology/onto_ontology.txt'))
 ANS2ID_DICT = {"open": ANSWER_VOCAB, "wiki": WIKI_VOCAB, "kb": KB_VOCAB, "onto":ONTO_ANS_VOCAB}
 
 open_id2ans = {v: k for k, v in ANSWER_VOCAB.items()}
@@ -34,6 +36,6 @@ LABEL = label_string("HEAD", "WIKI", "KB")
 
 CHAR_DICT = defaultdict(int)
 char_vocab = [u"<unk>"]
-with open(FILE_ROOT + "/ontology/char_vocab.english.txt") as f:
+with open(os.path.join(FILE_ROOT, "ontology/char_vocab.english.txt")) as f:
   char_vocab.extend(c.strip() for c in f.readlines())
   CHAR_DICT.update({c: i for i, c in enumerate(char_vocab)})
